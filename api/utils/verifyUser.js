4ff7ec7078d3914524d5558bacacf550dcc5e97c
Rami Abo Ajeeb <rami@zeour.co.uk>
2024-03-29 17:00:36 +0300

    complete update user functionality

--- api/utils/verifyUser.js
+++ api/utils/verifyUser.js
@@ -1,21 +1,15 @@
-import { errorHandler } from "./error.js";
-import  jwt  from "jsonwebtoken";
-export const verifyToken = (req,res,next) => {
-    const tolen = req.cookie.access_token;
+import jwt from 'jsonwebtoken';
+import { errorHandler } from './error.js';
 
-    if(!token)
-    {
-        return next(errorHandler(401,'Unauthorized!'));
-    }
+export const verifyToken = (req, res, next) => {
+  const token = req.cookies.access_token;
 
-    try{
-        jwt.verify(token,process.env.JWT_SECRET, (err,user)=>{
-            if(err) return next(errorHandler(403,'Forbidden!'));
-            req.user = user;
-            next();
-        })
-    }catch(eror)
-    {
+  if (!token) return next(errorHandler(401, 'Unauthorized'));
 
-    }
-}
+  jwt.verify(token, process.env.JWT_SECRET, (err, user) => {
+    if (err) return next(errorHandler(403, 'Forbidden'));
+
+    req.user = user;
+    next();
+  });
+};
