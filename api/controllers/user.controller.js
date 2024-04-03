cb1e5d48011111c112ca7caae3dd0c907f7e49f3
Rami Abo Ajeeb <rami@zeour.co.uk>
2024-04-03 14:23:47 +0300

    create get user listing api route

--- api/controllers/user.controller.js
+++ api/controllers/user.controller.js
@@ -1,11 +1,7 @@
 import User from "../models/user.model.js";
+import Listing from "../models/listing.model.js";
 import { errorHandler } from "../utils/error.js";
 import bcryptjs from "bcryptjs";
-export const test = (req,res) => {
-    res.json({
-        message:'hello from user controller!'
-    });
-}
 
 export const updateUser = async (req, res, next) => {
     if (req.user.id !== req.params.id)
@@ -45,6 +41,24 @@ export const deleteUser = async (req,res,next) => {
 
   }catch(error)
   {
+    next(error)
+  }
+}
+
+export const getUserListing = async (req,res,next) => {
+  try{
+    if(req.user.id == req.params.id)
+    {
+      try{
+        const listings = await Listing.find({userRef:req.params.id});
+        res.status(200).json(listings);
+      }catch(error){
+        next(error)
+      } 
+    }else{
+      return next(errorHandler(401,'You can only access your own listings!'))
+    }
+  }catch(error){
     next(error)
   }
 }
